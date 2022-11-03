190e1ad239af4d586834257e29ae6e3c6b215770
Iaroslav Uliantcev <doudou239@gmail.com>
2022-11-03 11:59:51 +0300

    add tests of async functions

---
+++ src/getData/getData.js
@@ -0,0 +1,17 @@
+// import axios from "axios";
+const axios = require('axios');
+
+const mapArrToString = require('../mapArrToString/mapArrToString')
+
+const getData = async ()=>{
+    try{
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+        const usersId = response.data.map(user => user.id)
+        return mapArrToString(usersId)
+    }catch (e) {
+
+    }
+
+ }
+
+ module.exports = getData;
